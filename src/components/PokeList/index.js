14f567b973ed03f93634ea4d97e6db1218f1778f
emma-martin <emmamartinalvarez@gmail.com>
2019-03-08 10:17:32 +0100

    no results found message added at PokeList

--- src/components/PokeList/index.js
+++ src/components/PokeList/index.js
@@ -5,26 +5,31 @@ class PokeList extends Component {
 
     render() {
         const {filterPokemons} = this.props;
-        return (
-            <ul className="app__list">
-                {filterPokemons.map((item, index) => {
-                    return (
-                        <li key={index} id={item.id} className="app__list-item">
-                            <div className="pokemon">
-                                <img src={item.img} alt={item.name} />
-                                <div className="pokemon__id">ID / {item.id}</div>
-                                <h2 className="pokemon__name">{item.name}</h2>
-                                {item.type.map((type, index) => {
-                                    return (
-                                        <span key={index} className="pokemon__types">{type}</span>
-                                    );
-                                })}
-                            </div>
-                        </li>
-                    );
-                })}
-            </ul>
-        );
+        if(filterPokemons.length === 0){
+            return <p>No matched results. Try to catch another pokemon</p>
+        } else {
+            return (
+                <ul className="app__list">
+                    {filterPokemons.map((item, index) => {
+                        return (
+                            <li key={index} id={item.id} className="app__list-item">
+                                <div className="pokemon">
+                                    <img src={item.img} alt={item.name} />
+                                    <div className="pokemon__id">ID / {item.id}</div>
+                                    <h2 className="pokemon__name">{item.name}</h2>
+                                    {item.type.map((type, index) => {
+                                        return (
+                                            <span key={index} className="pokemon__types">{type}</span>
+                                        );
+                                    })}
+                                </div>
+                            </li>
+                        );
+                    })}
+                </ul>
+            );
+        }
+        
     }
 }
 
