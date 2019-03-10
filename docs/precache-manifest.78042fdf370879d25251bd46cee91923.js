42d2c3b87a98f7281b3959db411a704891fca641
emma-martin <emmamartinalvarez@gmail.com>
2019-03-10 11:10:51 +0100

    build docs

---
+++ docs/precache-manifest.78042fdf370879d25251bd46cee91923.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "fd23a0aef8f7bca0d82c",
+    "url": "/static/js/main.b58e62ae.chunk.js"
+  },
+  {
+    "revision": "ac164eeb7fe39897100f",
+    "url": "/static/js/2.aa7a0768.chunk.js"
+  },
+  {
+    "revision": "fd23a0aef8f7bca0d82c",
+    "url": "/static/css/main.262c9062.chunk.css"
+  },
+  {
+    "revision": "7d3d548937c8a962c5f4e7739ee86f31",
+    "url": "/index.html"
+  }
+];
